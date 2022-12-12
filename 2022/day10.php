1d65e38965b67fb4d57badeb0a8cd11491b589a0
Jay Johnson <jayj@indeed.com>
2022-12-11 23:40:58 -0600

    Day 10

--- 2022/day10.php
+++ 2022/day10.php
@@ -1,12 +1,11 @@
 <?php
 
 global $lines;
+global $signal_sum;
 $lines = explode(PHP_EOL, file_get_contents('day10.in'));
 
-function getline($line_num) {
-    global $lines;
-    $cmds = explode(' ', $lines[$line_num]);
-    $line_num++;
+function getline($line) {
+    $cmds = explode(' ', $line);
     if (count($cmds)>1) {
         list($cmd, $num) = $cmds;
     } else {
@@ -16,42 +15,45 @@ function getline($line_num) {
     return [$cmd, $num];
 }
 
+function handle_tick($cycle, $x) {
+    global $signal_sum;
+    global $screen;
+
+    $t1 = $cycle-1;
+    echo "$cycle $t1 $x " . $t1%40 . " " . abs($x-($t1%40)) . "\n";
+    if (abs($x-($t1%40)) <= 1) {
+        $screen[$t1/40][$t1%40] = "#";
+    } else {
+        $screen[$t1/40][$t1%40] = " ";
+    }
+    if (in_array($cycle, [20, 60, 100, 140, 180, 220])) {
+        $signal_sum += ($cycle * $x);
+    }
+}
+
 $cycle = 0;
 $x = 1;
 $line_num = 0;
 $waiting = true;
 $eof = false;
 $signal_sum = 0;
+$screen = array_fill(0, 6, array_fill(0, 40, '?'));
 
-while (true) {
-    $cycle++;
-    if ($waiting) {
-        if ($line_num < count($lines)) {
-            list($cmd, $num) = getline($line_num);
-            $line_num++;
-        } else {
-            $eof = true;
-        }
-        $waiting = $cmd == 'noop';
-    } else {
-        if ($cmd == 'addx') {
-            $x += $num;
-        }
-        $waiting = true;
-        if ($eof) {
-            break;
-        }
+foreach ($lines as $line) {
+    list($cmd, $num) = getline($line);
+    if ($cmd == 'noop') {
+        $cycle++;
+        handle_tick($cycle, $x);
+    } elseif ($cmd == 'addx') {
+        $cycle++;
+        handle_tick($cycle, $x);
+        $cycle++;
+        handle_tick($cycle, $x);
+        $x += $num;
     }
+}
 
-    echo "$cycle, $cmd, $num, $x\n";
-
-    if (in_array($cycle, [20, 60, 100, 140, 180, 220])) {
-        echo $signal_sum . ' ' . ($cycle * $x) . PHP_EOL;
-        $signal_sum += ($cycle * $x);
-        echo $signal_sum . PHP_EOL;
-    }
-    if ($cycle > 230) {
-        echo $signal_sum . PHP_EOL;
-        break;
-    }
+echo $signal_sum . PHP_EOL;
+foreach ($screen as $sl) {
+    echo implode('', $sl) . PHP_EOL;
 }
