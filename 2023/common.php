2a703ee2c3e21eb4985918b0c71b49899eff01b0
Jay Johnson <magic976@gmail.com>
2023-12-08 20:27:37 -0600

    2023: Day 8

--- 2023/common.php
+++ 2023/common.php
@@ -1,21 +1,121 @@
 <?php
 
-$adjacents = [[1, 1], [1, 0], [0, -1], [-1, -1]];
-$adjacentdiags = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+$adjacents     = [
+	[
+		1,
+		1,
+	],
+	[
+		1,
+		0,
+	],
+	[
+		0,
+		-1,
+	],
+	[
+		-1,
+		-1,
+	],
+];
+$adjacentdiags = [
+	[
+		-1,
+		-1,
+	],
+	[
+		-1,
+		0,
+	],
+	[
+		-1,
+		1,
+	],
+	[
+		0,
+		1,
+	],
+	[
+		1,
+		1,
+	],
+	[
+		1,
+		0,
+	],
+	[
+		1,
+		-1,
+	],
+	[
+		0,
+		-1,
+	],
+];
 
-function get_input($day, $example = false) {
-    return explode(PHP_EOL, file_get_contents('day' . $day . ($example ? '.ex' : '.in')));
-}
 
-function inbounds($row, $column, $width, $height) {
-    return $row > 0 && $row < $width && $column > 0 && $column < $height;
-}
+function get_input($day, $example = false)
+{
+	return explode(PHP_EOL, file_get_contents('day'.$day.($example ? '.ex' : '.in')));
 
-function d($var) {
-    var_dump($var);
-}
+}//end get_input()
 
-function dd($var) {
-    var_dump($var);
-    die();
-}
+
+function inbounds($row, $column, $width, $height)
+{
+	return $row > 0 && $row < $width && $column > 0 && $column < $height;
+
+}//end inbounds()
+
+
+/**
+ * @param  int $num
+ * @return array The common factors of $num
+ */
+function getFactors($num)
+{
+	$factors = [];
+	// get factors of the numerator
+	for ($x = 1; $x <= $num; $x++) {
+		if (($num % $x) == 0) {
+			$factors[] = $x;
+		}
+	}
+	return $factors;
+
+}//end getFactors()
+
+
+/**
+ * @param int $x
+ * @param int $y
+ */
+function getGreatestCommonDenominator($x, $y)
+{
+	// first get the common denominators of both numerator and denominator
+	$factorsX = getFactors($x);
+	$factorsY = getFactors($y);
+	
+	// common denominators will be in both arrays, so get the intersect
+	$commonDenominators = array_intersect($factorsX, $factorsY);
+	
+	// greatest common denominator is the highest number (last in the array)
+	$gcd = array_pop($commonDenominators);
+	return $gcd;
+
+}//end getGreatestCommonDenominator()
+
+
+function d($var)
+{
+	var_dump($var);
+
+}//end d()
+
+
+function dd($var)
+{
+	var_dump($var);
+	die();
+
+}//end dd()
--- 2023/day8.php
+++ 2023/day8.php
@@ -1,69 +1,108 @@
 <?php
 
-include 'common.php';
+require 'common.php';
 
 $lines = get_input(8, false);
 $nodes = [];
 foreach ($lines as $k => $line) {
-    if ($k===0) {
-        $instructions = $line;
-        continue;
-    }
-    if (empty($line)) {
-        continue;
-    }
-    [$index, $next_elements] = explode(' = ', $line);
-    $next_elements = explode(', ', str_replace(['(',')'], '', $next_elements));
-    $nodes[$index] = $next_elements;
+	if ($k === 0) {
+		$instructions = $line;
+		continue;
+	}
+	if (empty($line)) {
+		continue;
+	}
+	[
+		$index,
+		$next_elements,
+	] = explode(' = ', $line);
+	$next_elements = explode(', ', str_replace(['(', ')'], '', $next_elements));
+	$nodes[$index] = $next_elements;
 }
 
 // $current_instruction = 0;
 // $current_node = 'AAA';
 // $steps = 0;
 // do {
-//     if ($current_instruction >= strlen($instructions)) {
-//         $instructions .= $instructions;
-//     }
-//     d($instructions);
-//     $dir = $instructions[$current_instruction];
-//     $current_node = $nodes[$current_node][$dir === 'L' ? 0 : 1];
-//     $steps++;
-//     $current_instruction++;
-//     echo $steps . PHP_EOL;
-
+// if ($current_instruction >= strlen($instructions)) {
+// $instructions .= $instructions;
+// }
+// d($instructions);
+// $dir = $instructions[$current_instruction];
+// $current_node = $nodes[$current_node][$dir === 'L' ? 0 : 1];
+// $steps++;
+// $current_instruction++;
+// echo $steps . PHP_EOL;
 // } while ($current_node !== 'ZZZ');
-
 // echo "part 1: $steps" . PHP_EOL;
+$anodes = array_filter(
+	$nodes,
+	function ($item, $k) {
+		return str_ends_with($k, 'A');
+	},
+	ARRAY_FILTER_USE_BOTH
+);
+$cycles = [];
+foreach ($anodes as $k => $anode) {
+	$cycle         = [];
+	$current_steps = $instructions;
+	$step_count    = 0;
+	$first_z       = '';
 
-$anodes = array_filter( $nodes, function($item, $k) {
-    return str_ends_with($k, 'A');
-}, ARRAY_FILTER_USE_BOTH );
+	while (true) {
+		$current = $k;
+		while ($step_count === 0 || ! str_ends_with($current, 'Z')) {
+			$step_count++;
+			$current       = $nodes[$current][$current_steps[0] === 'L' ? 0 : 1];
+			$current_steps = substr($current_steps, 1).$current_steps[0];
+			// d($current);
+			// echo $step_count.' '.$current_steps.PHP_EOL;
+		}
+		echo "Found a Z in step $step_count".PHP_EOL;
+		$cycle = $step_count;
 
-$current_instruction = 0;
+		if ($first_z === '') {
+			// echo 'Found first Z in '.$step_count.PHP_EOL;
+			$first_z    = $current;
+			$step_count = 0;
+			break;
+		} else if ($current === $first_z) {
+			break;
+		}
+	}//end while
+
+	$cycles[] = $cycle;
+}//end foreach
+d($cycles);
+
+$lcm = array_pop($cycles);
+foreach ($cycles as $cycle) {
+	$lcm = ($lcm * $cycle / gmp_gcd($lcm, $cycle));
+}
+dd($lcm);
 $steps = 0;
-do {
-    if ($current_instruction >= strlen($instructions)) {
-        $instructions .= $instructions;
-    }
-    $dir = $instructions[$current_instruction];
+while (true) {
+	if ($current_instruction >= strlen($instructions)) {
+		$instructions .= $instructions;
+	}
+	$dir = $instructions[$current_instruction];
 
-    $new_anodes = [];
-    foreach($anodes as $k => $anode) {
-        $new_index = $nodes[$k][$dir === 'L' ? 0 : 1];
-        $new_nodes = $nodes[$new_index];
-        $new_anodes[$new_index] = $new_nodes;
-    }
-    $anodes = $new_anodes;
-    $steps++;
-    $current_instruction++;
-    echo $steps . PHP_EOL;
-    $continue = false;
-    foreach($anodes as $k => $anode) {
-        if (!str_ends_with($k, 'Z')) {
-            $continue = true;
-        }
-    }
-    if (!$continue) {
-        break;
-    }
-} while (true);
+	$new_anodes = [];
+	foreach ($anodes as $k => $anode) {
+		$new_index = $nodes[$k][$dir === 'L' ? 0 : 1];
+		$new_nodes = $nodes[$new_index];
+		$new_anodes[$new_index] = $new_nodes;
+	}
+	$anodes = $new_anodes;
+	$steps++;
+	echo $steps.PHP_EOL;
+	$continue = false;
+	foreach ($anodes as $k => $anode) {
+		if (!str_ends_with($k, 'Z')) {
+			$continue = true;
+		}
+	}
+	if (!$continue) {
+		break;
+	}
+}//end while
